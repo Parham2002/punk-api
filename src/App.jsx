08691131568be4c295193269ab3e860a696bf021
Parham2002 <parhamm2002@gmail.com>
2022-09-12 15:45:28 +0100

    Added frames for header, main, and nav on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.scss';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Nav/>
+      <Main/>
 
     </div>
   );
