52b6963a35efaebbe4ba7a3d8c1d783159dd38d6
Parham2002 <parhamm2002@gmail.com>
2022-09-16 12:02:21 +0100

    filter search is actually functional now...

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,23 +10,21 @@ const Main = ({searchTerm}) => {
     const url = "https://api.punkapi.com/v2/beers";
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data)
-    setBeers(data)
-  }
-  getBeers()
-  
- 
-useEffect(() => {
-    if(beers.length === 0 ){
-      return 
-    }
-    const filterSearch = beers.filter((beer) => beer.name.includes(searchTerm))
+    const filterSearch = data.filter((beer) => beer.name.includes(searchTerm))
     setBeers(filterSearch)
-  }, [searchTerm])
+  }
+  
+  useEffect(() => {
+    getBeers()
+
+  },[searchTerm])
+ 
+  
+
 
   return (
     <div className='main'>
-      {beers?.map((beer) => {
+      {beers.map((beer) => {
         return (<Card key={beer.id} data={beer}/>)
       })}
     </div>
